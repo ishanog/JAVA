413c3e73749ce7d0a4473577458e1b40e959e00c
Ishan <ishanshrestha288@gmail.com>
2025-07-15 08:46:01 +0545

    .

--- WAP10_Student.java
+++ WAP10_Student.java
@@ -16,4 +16,11 @@ class WAP10_Student {
         this.id = s.id;
         this.marks = s.marks;
     }
+    public static void main(String[] args) {
+
+        WAP10_Student s1 = new WAP10_Student(); 
+
+        System.out.println("Student: ID=" + s1.id + ", Marks=" + s1.marks);
+    }
 }
+
